e80d1665f00d006e1bd32b96ca9e4f6aea7a0a13
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-10-22 09:44:54 +0000

    Sat, Oct 22, 2022, 9:44 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,6 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int main(void)
+{
+    int height;
+    int spaces;
+    int hashes;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
  for (int i = 0; i < n; i++)
 {
     for (int j = 0; j < n; j++)
