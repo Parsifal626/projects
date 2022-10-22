6e98d5183b0218a4119f60f023895d2d7786dd4c
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-10-22 09:46:51 +0000

    Sat, Oct 22, 2022, 9:46 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,7 +11,9 @@ int main(void)
     {
         height = get_int("Height: ");
     }
- for (int i = 0; i < n; i++)
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i < height; i++)
 {
     for (int j = 0; j < n; j++)
     {
