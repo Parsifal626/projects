0e9b8cbda2e1e805c60cef9a96a1f20f2ee40575
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-10-22 09:35:58 +0000

    Sat, Oct 22, 2022, 9:35 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,12 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
  for (int i = 0; i < n; i++)
 {
     for (int j = 0; j < n; j++)
     {
-            printf("#");
+        printf("#");
     }
     printf("\n");
 }
