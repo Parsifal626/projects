08588d802cfb0c70e9a40da4619334a275ec7a3d
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-03 12:34:32 +0000

    Thu, Nov 3, 2022, 12:34 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -40,7 +40,7 @@ int compute_score(string word)
 char alphabet[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 {
     int score = 0;
-    for (int n = 0; n < strlen(word), n++)
+    for (int n = 0; n < strlen(word); n++)
     {
         if (isupper(word[n]))
         {
