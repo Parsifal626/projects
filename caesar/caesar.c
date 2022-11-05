5a7e3647dbd9a5c1dd1486955965e7c1b859ce63
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-05 07:16:35 +0000

    Sat, Nov 5, 2022, 7:16 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -34,7 +34,7 @@ if (argc != 2)
 
             int plainLength = strlen(plain);
 
-            for(int i = 0; i , plainLength; i++)
+            for(int i = 0; i < plainLength; i++)
             {
                 if (isupper(plain[i]))
                 {
@@ -43,21 +43,54 @@ if (argc != 2)
                      {
                         int keyRemainder = (plain[i] + KEY) - 'Z';
 
+                        if (keyRemainder > 'Z' - 'A')
+                        {
+                            while (keyRemainder >= ('Z' - 'A'))
+                            {
+                                keyRemainder = keyRemainder - ('Z' - 'A');
+                            }
+                            if (plain[i] + keyRemainder > 'Z')
+                            {
+                                keyRemainder = plain[i] + keyRemainder - "Z";
+                                plain[i] = 'A' + keyRemainder - 1;
+                            }
+                            else
+                            {
+                                plain[i] = 'a' + keyRemainder - 1;
+                            }
+                            }
+                            else
+                            {
+                                plain[i] = 'A' + keyRemainder - 1;
+                            }
+                        }
+                     else if (plain[i] + KEY <= 'Z')
+                     {
+                        plain[i] = plain[i] + KEY
+                     }
+                 }
+                if (islower(plain[i]))
+                {
+
+                     if (plain[i] + KEY > 'z')
+                     {
+                        int keyRemainder = (plain[i] + KEY) - ('z');
+
                         if (keyRemainder > 'z' - 'a')
                         {
                             while (keyRemainder >= ('z' - 'a'))
                             {
-                                keyRemainder = keyRemainder - ('z' - 'a');
+                                keyRemainder = keyRemainder - (26);
                             }
                             if (plain[i] + keyRemainder > 'z')
                             {
                                 keyRemainder = plain[i] + keyRemainder - "z";
-
                                 plain[i] = 'a' + keyRemainder - 1;
                             }
                             else
                             {
                                 plain[i] = 'a' + keyRemainder - 1;
+                            }
                             }
                             else
                             {
@@ -70,19 +103,12 @@ if (argc != 2)
                      }
                  }
            }
-
-
+                printf("ciphertext: %s\n", plain);
      }
      else
     {
         printf("Usage: ./caesar key\n");
     }
-
-}
-
-                else if (islower(plain[i]))
-
-                printf("ciphertext: %s\n", plain);
             }
             else
             {
