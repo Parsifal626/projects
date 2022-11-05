b21ac18451c17e1cea386bac89ea0de560382af3
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-05 06:19:21 +0000

    Sat, Nov 5, 2022, 6:19 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,6 +9,13 @@ int main(int argc, string argv[])
 if (argc != 2)
     {
         printf("Usage: ./caesar key.\");
+        printf("1");
+        return(1)
+    }
+
+    else if (argc == 2)
+    {
+         
     }
         for (int i = 0; i < strlen(argv[1]); i++)
         {
