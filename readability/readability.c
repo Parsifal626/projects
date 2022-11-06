4f1d99d0f57ba4f548762c08baf4849f575f3b1b
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-06 16:24:46 +0000

    Sun, Nov 6, 2022, 4:24 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,8 +5,15 @@ int main(void)
 {
     string text = get_string("Text: ");
 
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+
     for (int = 0; i < strlen(text); i++)
     {
-        if 
+        if ((text[i] > 65 && text[i] < 90) || (text[i] > 97 && text[i] < 122))
+        {
+            letters++;
+        }
     }
 }
