a43b209db2974647849948b6b2ca336610be23d8
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-06 16:53:54 +0000

    Sun, Nov 6, 2022, 4:53 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -34,15 +34,15 @@ int main(void)
 
     if (index < 1)
     {
-       printf("Before Grade 1");
+       printf("Before Grade 1\n");
     }
     if (index > 16)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
 
     else
     {
-        printf("Grade %i", index);
+        printf("Grade %i"\n, index);
     }
 }
