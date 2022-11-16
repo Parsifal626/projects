52098431b9be2f82f126056a85b2c2962ff2e409
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-16 14:01:02 +0000

    Wed, Nov 16, 2022, 2:01 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,6 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    bool exist = false;
     for (int i = 0; i < candidate_count; i++)
     {
         if(strcmp(name, candidates[i].name) == 0)
