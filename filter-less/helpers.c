854d8aecf20a60928680c84db5d15b2083d556cf
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-20 17:36:54 +0000

    Sun, Nov 20, 2022, 5:36 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -13,7 +13,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             float Green = image[i][j].rgbtGreen;
 
             int average = round((Red + Green + Blue) / 3);
-            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j]rgbtBlue = average;
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
 
         }
     }
