bee3371d6a030106dcab812d60b5e91c6886ae18
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-20 17:23:39 +0000

    Sun, Nov 20, 2022, 5:23 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int blue = image[i][j].rgbtBlue;
+            int green = image[i][j].rgbtGreen;
+        }
+    }
     return;
 }
 
