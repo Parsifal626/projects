b9132efb1b750da33ba534ee0f6231577de683b7
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-21 08:58:50 +0000

    Mon, Nov 21, 2022, 8:58 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdint.h>
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc !=2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+    else
+    {
+        printf("Error: cannot open %s\n", input_file_name);
+        return 2;
+    }
+
 }
