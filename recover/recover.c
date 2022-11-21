e81142408f033c98452d718d2520b81f6167cf24
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-21 17:04:34 +0000

    Mon, Nov 21, 2022, 5:04 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -33,17 +33,18 @@ int main(int argc, char *argv[])
             have_found_first_jpg = true;
             else
                 fclose(output_file);
+
                 char filename[FILE_NAME_SIZE];
                 sprint(filename, "%03i.jpg", file_index++);
                 output_file = fopen(filename, "w");
-                if (outfile == NULL)
+                if (output_file == NULL)
                 return 1;
-                fwrite(buffer, BLOCK_SIZE, 1, outfile);
+                fwrite(buffer, BLOCK_SIZE, 1, output_file);
 
 
         }
         else if (have_found_first_jpg)
-        fwrite(buffer, BLOCK_SIZE, 1, outfile);
+        fwrite(buffer, BLOCK_SIZE, 1, output_file);
     }
 }
 
@@ -51,8 +52,6 @@ int main(int argc, char *argv[])
         fclose(output_file);
 
 
-
-}
 bool is_start_new_jpeg(BYTE buffer[])
 {
     return (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
