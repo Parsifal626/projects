fac55ccf88b1efc343abe8dc64b2989e83a88818
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-11-21 09:07:24 +0000

    Mon, Nov 21, 2022, 9:07 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,5 +21,32 @@ int main(int argc, char *argv[])
             return 2;
         }
     }
-
-}
+    BYTE buffer[512];
+    int count = 0;
+    FILE *img_pointer = NULL;
+    char filename[8];
+    while (fread(&buffer, 512, 1, img_pointer) == 1)
+        {
+            // If start of a new JPEG (0xff 0xd8 0xff 0xe*):
+            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+            {
+                // If not first JPEG, close previous
+                if (!(count == 0))
+                {
+                    fclose(img_pointer);
+                }
+                // Initialise file
+                sprintf(filename, "%03i.jpg", count);
+                img_pointer = fopen(filename, "w");
+                count++;
+            }
+            // If JPEG has been found, write to file
+            if (!(count == 0))
+            {
+                fwrite(&buffer, 512, 1, img_pointer);
+            }
+        }
+        fclose(img_pointer);
+        fclose(img_pointer);
+        return 0;
+    }
