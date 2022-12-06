e228501b2ad67922d307c700bcf2f4af37ba696c
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-12-06 14:48:17 +0000

    Tue, Dec 6, 2022, 2:48 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,16 @@
 # TODO
+
+from cs50 import get_string
+
+text = get_string("Text: ")
+
+letters = 0
+words = 1
+sentences = 0
+
+for i in text:
+    if i.isalpha():
+        letters +=1
+    elif i == " ":
+        words +=1
+        elif == "." or i == "!" or i == "?"
