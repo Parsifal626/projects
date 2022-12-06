953b863a6262244dbb1ecd5c3ee8189bb07de7ce
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2022-12-06 14:53:28 +0000

    Tue, Dec 6, 2022, 2:53 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -13,7 +13,7 @@ for i in text:
         letters +=1
     elif i == " ":
         words +=1
-        elif == "." or i == "!" or i == "?"
+        elif == "." or i == "!" or i == "?":
         sentences +=1
 
 index = 0.0588 * (letters/words*100) - 0.296 * (sentences / words*100) - 15.8
@@ -24,4 +24,3 @@ elif index >= 16:
     print("Grade 16+")
 else:
     print("Grade ", round (index))
-    
