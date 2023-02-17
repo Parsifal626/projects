7421a9e06a38d636d71a471f087b7dfb04f1cf67
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-17 14:42:02 +0000

    Fri, Feb 17, 2023, 2:42 PM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,20 +1,5 @@
 s = input(str('Type something.  '))
-
-g = 
-
-def replace(old, new):
-    for i in s:
-        if i == (':)') and
-
-
-
- if s(':)') in s:
-    print(s.replace(':)', '🙂'))
-elif s(':(') in s:
-    print(s.replace(':(', '🙁'))
-else:
-    print(s)
-
+print(s.replace(':)', '🙂').replace(':(', '🙁'))
 
 
 
