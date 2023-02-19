8c991932c023ac1f77cba39e4de37048ec4dd40e
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-19 13:39:00 +0000

    Sun, Feb 19, 2023, 4:39 PM +03:00

---
+++ deep/deep.py
@@ -0,0 +1,7 @@
+def main(s):
+    s = input('What is the Answer to the Great Question of Life, the Universe, and Everything? ')
+    l = l.lower(('forty two'), l.upper('forty two'), l.split('-', 'forty two'))
+        if s is 42:
+            print('Yes')
+        else:
+        print('No')
