335f5c17b6428f1751bf56c397918f72c30a6367
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-19 13:47:00 +0000

    Sun, Feb 19, 2023, 4:47 PM +03:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,7 @@
 def main():
 
     s = input('What is the Answer to the Great Question of Life, the Universe, and Everything? ')
-           if i == ("42" or "forty two" or "forty-two"):
+        if s == ("42" or "forty two" or "forty-two"):
             print("Yes")
         else:
             print('No')
