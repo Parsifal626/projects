5783df7162628bac52751a109910ebc6c979c60e
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 07:41:46 +0000

    Tue, Feb 21, 2023, 10:41 AM +03:00

--- test_sber/playlist.py
+++ test_sber/playlist.py
@@ -33,14 +33,8 @@ class Playlist:
             self.__play_thread.join()
             self.__play_thread_lock.release()
 
+        self.__current_song_index ==1
 
-
-    def next_song(self):
-        if self.__play_thread is not None:
-            self.__play_thread_lock.acquire()
-            self.__is_playing = False
-            self.__play_thread.join()
-            self.__play_thread_lock.release()
 
         self.__current_song_index += 1
         if self.__current_song_index >= len(self.__songs):
