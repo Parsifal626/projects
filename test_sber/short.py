09b6cc4c627901ade338ffaebfd77690aed6c4a3
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 07:11:12 +0000

    Tue, Feb 21, 2023, 10:11 AM +03:00

---
+++ test_sber/short.py
@@ -0,0 +1,15 @@
+class Playlist:
+    def play(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def add_song(self, song):
+        pass
+
+    def next(self):
+        pass
+
+    def prev(self):
+        pass
