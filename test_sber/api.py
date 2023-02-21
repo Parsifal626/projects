4786007acc8b3a9d4858a3d5b323047f9c5238d7
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 15:59:03 +0000

    Tue, Feb 21, 2023, 6:59 PM +03:00

---
+++ test_sber/api.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
+db = SQLAlchemy(app)
+
+class Track(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    artist = db.Column(db.String(50), nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'Track(id={self.id}, title={self.title}, artist={self.artist}, duration={self.duration})'
+
+@app.route('/tracks', methods=['GET'])
+def get_all_tracks():
+    tracks = Track.query.all()
+    return jsonify([track.__dict__ for track in tracks]), 200
+
+@app.route('/tracks/<int:track_id>', methods=['GET'])
+def get_track(track_id):
+    track = Track.query.filter_by(id=track_id).first()
+    if track:
+        return jsonify(track.__dict__), 200
+    else:
+        return jsonify({'message': 'Track not found'}), 404
+
+@app.route('/tracks', methods=['POST'])
+def create_track():
+    data = request.get_json()
+    track = Track(title=data['title'], artist=data['artist'], duration=data['duration'])
+    db.session.add(track)
+    db.session.commit()
+    return jsonify(track.__dict__), 201
+
+@app.route('/tracks/int:track_id', methods=['PUT'])
+def update_track(track_id):
+    track = Track.query.filter_by(id=track_id).first()
+    if track:
+        data = request.get_json()
+        track.title = data.get('title', track.title)
+        track.artist = data.get('artist', track.artist)
+        track.duration = data.get('duration', track.duration)
+        db.session.commit()
+        return jsonify(track.dict), 200
+    else:
+        return jsonify({'message': 'Track not found'}), 404@app.route('/tracks/int:track_id', methods=['DELETE'])
+def delete_track(track_id):
+    track = Track.query.filter_by(id=track_id).first()
+    if track:
+        db.session.delete(track)
+        db.session.commit()
+        return jsonify({'message': 'Track deleted successfully'}), 200
+    else:
+        return jsonify({'message': 'Track not found'}), 404 if name == 'main':
+app.run(debug=True)
