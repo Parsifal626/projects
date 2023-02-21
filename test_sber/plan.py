844e9e9093fc34fec0266139593ae33e77db9cdb
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 07:11:46 +0000

    Tue, Feb 21, 2023, 10:11 AM +03:00

--- test_sber/plan.py
+++ test_sber/plan.py
@@ -1,18 +1,3 @@
-class Playlist:
-    def play(self):
-        pass
-
-    def pause(self):
-        pass
-
-    def add_song(self, song):
-        pass
-
-    def next(self):
-        pass
-
-    def prev(self):
-        pass
 # Для реализации двусвязного списка, который будет использоваться для хранения плейлиста, можно создать отдельный класс:
 class SongNode:
     def __init__(self, song):
