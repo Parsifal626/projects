c00f10398dbaf57f1368832c8b658991e4e6d1a8
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 11:47:13 +0000

    Tue, Feb 21, 2023, 2:47 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -7,8 +7,8 @@ if ("jpg" or "jpeg") in s:
     print('image/jpeg')
 if ("png") in s:
     print('image/png')
-elif 'png' in s:
-    print('image/png')
+elif 'gif' in s:
+    print('image/gif')
 elif "pdf" in s:
     print('application/pdf')
 elif "zip" in s:
