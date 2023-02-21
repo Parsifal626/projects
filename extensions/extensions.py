b4526f240d24c907349909e43127d79b6c2c63d2
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 11:53:14 +0000

    Tue, Feb 21, 2023, 2:53 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,7 +5,7 @@ s = input("Type your file " ).lower()
 
 if (".jpg" or ".jpeg") in s:
     print('image/jpeg')
-if (".png") in s:
+elif (".png") in s:
     print('image/png')
 elif 'gif' in s:
     print('image/gif')
@@ -13,6 +13,8 @@ elif ".pdf" in s:
     print('application/pdf')
 elif ".zip" in s:
     print('application/zip')
+elif ".txt" in s:
+    print('plain/text')
 else:
     print('application/octet-stream')
 
