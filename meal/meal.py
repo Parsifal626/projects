02f7ad807d5be6f9722a4e377899e5a1a15dc234
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 12:46:26 +0000

    Tue, Feb 21, 2023, 3:46 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,15 +1,24 @@
 def main():
     time = input("What time is it? ")
     hours, minutes = time.split(':')
-    hours = int(hours)
-    minutes = int(minutes)
-    if 
+    hours = float(hours)
+    minutes = float(minutes)
+
 
 
 
 
 def convert(time):
-    ...
+    time = [hours, minutes]
+    minutes = minutes / 60
+    if hours > 12:
+        hours = return(hours - 12)
+    else:
+        hours = return(hours)
+
+
+
+
 
 
 if __name__ == "__main__":
