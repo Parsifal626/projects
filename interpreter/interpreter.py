fd918f8bc9e6be8bc10850fcfadffb400e55be7b
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-21 12:17:54 +0000

    Tue, Feb 21, 2023, 3:17 PM +03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,5 +1,7 @@
-expression = float(input( 'Type xyz '))
-expression = (expression.split(' '))
+x = int(input( 'Type x '))
+y = int(input( 'Type y '))
+z = (+) or (-) or (*) or (/)
 
 
-print (f'{x:.2f}')
+
+print (f'{x z y.2f}')
