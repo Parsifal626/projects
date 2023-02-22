9872b0625e3d5392a6c04d3b1d383952bba86b74
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 10:33:26 +0000

    Wed, Feb 22, 2023, 1:33 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,6 +4,6 @@ s = ()
 
 while  insert() <= amount_due():
      if insert == 5:
-         amount_due() - 5
+         s= amount_due() - 5
      s = amount_due() - insert()
         print ("Amount Due:" s)
