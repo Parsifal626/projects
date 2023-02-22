b29630e67368a782d79ae160a9fc33554d655b7e
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 12:03:07 +0000

    Wed, Feb 22, 2023, 3:03 PM +03:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,14 +1,4 @@
-s = int(input("Type Something: "))
-for i in s:
-   
+s = (input("Type Something: ")).replace("A, E, I, O, U)", "")
+print (s)
 
 
-
-amount_due = 50
-while amount_due > 0:
-   print(f'Amount Due: {amount_due}')
-   coin = int(input("Insert Coin: "))
-   if coin in [25,10,5]:
-      amount_due -= coin
-change = abs(amount_due)
-print(f'Change Owed: {change}')
