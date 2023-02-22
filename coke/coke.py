befa2f4a7fcba880bdd2883b759f509a63467e2d
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 11:52:19 +0000

    Wed, Feb 22, 2023, 2:52 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,8 +1,9 @@
 amount_due = 50
 while amount_due > 0:
    print(f'Amount Due: {amount_due} ')
-   coin = int(input("Insert coin: "))
+   coin = int(input("Insert Coin: "))
    if coin in [25,10,5]:
       amount_due -= coin
+      print(f'Amount Due: {amount_due} ')
 change = abs(amount_due)
 print(f'Change Owed: {change}')
