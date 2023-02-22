3a3ca2c1f675982bdd1eb1affadae400bde64e61
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 11:03:49 +0000

    Wed, Feb 22, 2023, 2:03 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,20 +3,10 @@ def coincal(insert,amount_due)
        amount_due = amount_due - insert
        insert= int(input("Insert coin: "))
        if amount_due - insert == 0:
-           print('Changed owed:0')
+           print('Change owed:0')
         elif amount_due > insert:
-           print(f'Changed owed: {amount_due - insert}')
+           print(f'Amount due: {amount_due - insert}')
     if insert > amount_due:
-        print
+        print(f'Change owed: {insert - amount_due})
 
-
-
-
- = 50
-s = ()
-
-while  insert() <= amount_due():
-     if insert == 5:
-         s= amount_due() - 5
-     s = amount_due() - insert()
-        print ("Amount Due:" s)
+main()
