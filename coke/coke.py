26aafb7bc0d4ac6fe2ab0debe3592725e3d28d51
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 10:30:49 +0000

    Wed, Feb 22, 2023, 1:30 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,6 +2,6 @@ insert = int(input("Insert coin: "))
 amount_due = 50
 s = ()
 
-if  insert() <= amount_due():
+while  insert() <= amount_due():
      s() = amount_due() - insert()
-        return s()
+        print("Amount Due:" s())
