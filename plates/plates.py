4febd55ecac912530f44b307f42cbe115d303865
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 13:09:32 +0000

    Wed, Feb 22, 2023, 4:09 PM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -15,6 +15,11 @@ def is_valid(s):
     if s[0].isalpha() == False or s[1].isalpha() == False:
         return False
 
+    i=0
+    while i < len(s):
+        if s[i].isalpha() == False:
+            
+
     (len(s[0])+ len(s))
 
 
