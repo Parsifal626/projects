91c81839f65a5524a49d895522ac7f1a56edb326
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 12:29:15 +0000

    Wed, Feb 22, 2023, 3:29 PM +03:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,6 @@
 s = (input("Type Something: "))
 for i in s:
-    if not i in ["a", "e", "i", "o", "u"]:
+    if not i in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]:
         print(i, end="")
 
 
