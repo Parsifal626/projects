6fd9ec5c9776c0352dde769bfa37d9132e400889
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 12:44:49 +0000

    Wed, Feb 22, 2023, 3:44 PM +03:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,6 +10,7 @@ def is_valid(s):
     # min 2 and max 6 characters
     s = input("Type your vanity place. (min 2 and max 6): ")
     # minimum 2 letters
+    
     # mid = Letters
     # the first characters not 0
     # No periods, spaces, or punctuation marks are allowed
