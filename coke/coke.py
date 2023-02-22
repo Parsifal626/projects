6cc4482f1c14030ede7fca2f7eb5265cc32c7747
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-22 11:22:49 +0000

    Wed, Feb 22, 2023, 2:22 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,41 +4,5 @@ while amount_due > 0:
    insert= int(input("Insert coin: "))
    if insert in [5,10,25]:
       amount_due = amount_due - insert
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def main():
-    print(f'Amount due: 50')
-    insert= int(input("Insert coin: "))
-    amount_due = 50
-    while insert not in [5,10,25]:
-       print(f'Amount due: 50')
-       insert= int(input("Insert coin: "))
-    print(f'Amount due: {amount_due - insert}')
-    coincal(insert, amount_due)
-
-def coincal(insert,amount_due):
-    insert= int(input("Insert coin: "))
-    while amount_due > insert:
-       amount_due = amount_due - insert
-       insert= int(input("Insert coin: "))
-       if amount_due - insert == 0:
-        print('Change owed: 0')
-       elif amount_due > insert:
-        print(f'Amount due: {amount_due - insert}')
-    if insert > amount_due:
-        print(f'Change owed: {insert - amount_due}')
-
-main()
+change = abs(amount_due)
+print('Change owed: ', change)
