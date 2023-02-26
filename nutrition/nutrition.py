ebcbf0bb18a07af70ae6c6eb2af3ac11958cd4e2
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-26 17:10:09 +0000

    Sun, Feb 26, 2023, 8:10 PM +03:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,5 +1,13 @@
+def main():
+    
+
+
+s = input('Type fruit ').lower
+
+
 fruits = [
-    {'fruits': "Apple", 'calories' : 130,
-    {'fruits':"Avocado", 'calories': 50,
-     'fruits' ; "Banana", 'calories': 110, }
+    {'fruits': "Apple", 'calories' : '130'}
+    {'fruits': "Avocado", 'calories': '50'}
+    {'fruits': "Banana", 'calories': '110',}
+    {'fruits': "Sweet Cherries" , 'calories': '100'}
 ]
