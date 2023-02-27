2be953547c84d8c3a95731f78dab251940828b0f
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-27 08:54:25 +0000

    Mon, Feb 27, 2023, 11:54 AM +03:00

--- playlist/api.py
+++ playlist/api.py
@@ -1,46 +1,88 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'playlist.db'
 
-@app.route('/api/songs.db', methods=['GET'])
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/api/songs', methods=['GET'])
 def get_songs():
-    songs = ['Song 1', 'Song 2']
-    # код для получения списка песен из конфигурационного файла
-    # возвращает список песен в формате JSON
+    db = get_db()
+    cur = db.execute('SELECT * FROM songs')
+    songs = cur.fetchall()
     return jsonify(songs)
+
+@app.route('/api/songs/<int:id>', methods=['GET'])
+def get_song(id):
+    db = get_db()
+    cur = db.execute('SELECT * FROM songs WHERE id = ?', (id,))
+    song = cur.fetchone()
+    return jsonify(song)
 
 @app.route('/api/songs', methods=['POST'])
 def add_song():
-    # код для добавления песни в конфигурационный файл
-    # возвращает статус 201 (Created) в случае успеха
-    return '', 201
+    title = request.json['title']
+    artist = request.json['artist']
+    duration = request.json['duration']
 
-@app.route('/api/songs/<int:song_id>', methods=['PUT'])
-def update_song(song_id):
-    # код для обновления песни в конфигурационном файле
-    # возвращает статус 204 (No Content) в случае успеха
-    return '', 204
+    db = get_db()
+    db.execute('INSERT INTO songs (title, artist, duration) VALUES (?, ?, ?)',
+               [title, artist, duration])
+    db.commit()
 
-@app.route('/api/songs/<int:song_id>', methods=['DELETE'])
-def delete_song(song_id):
-    # код для удаления песни из конфигурационного файла
-    # возвращает статус 204 (No Content) в случае успеха
-    return '', 204
+    return jsonify({'result': True})
 
-@app.route('/api/play', methods=['POST'])
-def play_song():
-    # код для воспроизведения песни
-    # возвращает статус 200 (OK) в случае успеха
-    return '', 200
+@app.route('/api/songs/<int:id>', methods=['PUT'])
+def update_song(id):
+    title = request.json['title']
+    artist = request.json['artist']
+    duration = request.json['duration']
 
-@app.route('/api/pause', methods=['POST'])
-def pause_song():
-    # код для приостановки воспроизведения песни
-    # возвращает статус 200 (OK) в случае успеха
-    return '', 200
+    db = get_db()
+    db.execute('UPDATE songs SET title = ?, artist = ?, duration = ? WHERE id = ?',
+               [title, artist, duration, id])
+    db.commit()
 
-@app.route('/api/next', methods=['POST'])
-def next_song():
-    # код для перехода к следующей песне
-    # возвращает статус 200 (OK) в случае успеха
-    return '', 200
+    return jsonify({'result': True})
+
+@app.route('/api/songs/<int:id>', methods=['DELETE'])
+def delete_song(id):
+    db = get_db()
+    db.execute('DELETE FROM songs WHERE id = ?', [id])
+    db.commit()
+
+    return jsonify({'result': True})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    def get_all_songs(self) -> List[Song]:
+    """Возвращает все песни в плейлисте"""
+    return self._playlist.get_all_songs()
+
+def add_song(self, song_data: dict) -> Song:
+    """Добавляет новую песню в плейлист"""
+    song = Song(**song_data)
+    self._playlist.add_song(song)
+    return song
+
