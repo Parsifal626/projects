d35db5bba6196326efed19ae03b2ae54fe76fe9e
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-02-27 08:30:31 +0000

    Mon, Feb 27, 2023, 11:30 AM +03:00

--- playlist/playlist.py
+++ playlist/playlist.py
@@ -1,80 +1,51 @@
 import threading
 import time
 
-class Song:
-    def __init__(self, title, duration):
-        self.title = title
-        self.duration = duration
-
 class Playlist:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.current_node = None
+        self.songs = []
+        self.current_song_index = -1
         self.playing = False
         self.pause = False
-        self.lock = threading.Lock()
+        self.next_song_event = threading.Event()
 
-    def add_song(self, title, duration):
-        song = Song(title, duration)
-        node = PlaylistNode(song)
-
-        with self.lock:
-            if self.head is None:
-                self.head = node
-                self.tail = node
-            else:
-                self.tail.next_node = node
-                node.prev_node = self.tail
-                self.tail = node
+    def add_song(self, song):
+        self.songs.append(song)
 
     def play(self):
-        with self.lock:
-            if not self.playing and not self.pause:
-                self.playing = True
-                self.current_node = self.head
-                self._play_song(self.current_node.song)
-
-    def pause(self):
-        with self.lock:
-            if self.playing and not self.pause:
-                self.pause = True
-
-    def resume(self):
-        with self.lock:
-            if self.playing and self.pause:
-                self.pause = False
-                self._play_song(self.current_node.song)
-
-    def next(self):
-        with self.lock:
-            if self.current_node.next_node is not None:
-                self._stop_song()
-                self.current_node = self.current_node.next_node
-                self._play_song(self.current_node.song)
-
-    def prev(self):
-        with self.lock:
-            if self.current_node.prev_node is not None:
-                self._stop_song()
-                self.current_node = self.current_node.prev_node
-                self._play_song(self.current_node.song)
-
-    def _play_song(self, song):
-        t = threading.Thread(target=self._play, args=(song,))
+        if self.playing:
+            return
+        if self.current_song_index == -1:
+            self.current_song_index = 0
+        song = self.songs[self.current_song_index]
+        self.playing = True
+        self.pause = False
+        duration = min(song.duration, song.Duration)
+        t = threading.Thread(target=self._play_song, args=(duration,))
         t.start()
 
-    def _play(self, song):
-        time.sleep(song.duration)
-        with self.lock:
-            if not self.pause:
-                self.next()
+    def _play_song(self, duration):
+        time.sleep(duration)
+        self.playing = False
+        self.pause = False
+        self.next_song_event.set()
 
-    def _stop_song(self):
-        pass
+    def pause(self):
+        self.pause = True
 
-class PlaylistNode:
-    def __init__(self, song):
-        self.song = song
-        self.prev_node = None
-        self.next_node = None
+    def resume(self):
+        self.pause = False
+
+    def next_song(self):
+        if self.playing:
+            self.pause = True
+            self.next_song_event.set()
+        self.current_song_index = (self.current_song_index + 1) % len(self.songs)
+        self.play()
+
+    def prev_song(self):
+        if self.playing:
+            self.pause = True
+            self.next_song_event.set()
+        self.current_song_index = (self.current_song_index - 1) % len(self.songs)
+        self.play()
