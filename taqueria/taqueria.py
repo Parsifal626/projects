8eb51e37fd216d6162a0eb791cb966cbf038022d
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-01 11:14:23 +0000

    Wed, Mar 1, 2023, 2:14 PM +03:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,26 +1,28 @@
 def main ():
 
-d = {
-    "Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-        }
-amount = 0
+    d = {
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+            }
+    amount = 0
     while True:
-        try:
-            s = input("Item: ").title()
-            if s in d.key:
-                amount += d[item]
-                print ('Total: $', end = '')
-        except (EOFError, KeyError):
-            pass
-    print(f'{d[s.values]}%')
+            try:
+                s = input("Item: ").title()
+                if s in d:
+                    amount += d[s]
+                    print ('Total: $', end = '')
+                    print (amount)
+            except (EOFError, KeyError):
+                pass
+
+main()
 
 
 
