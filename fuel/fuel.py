8843cba9ced81c7e9961012a7dbcace563a95a2f
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-01 09:52:22 +0000

    Wed, Mar 1, 2023, 12:52 PM +03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,5 +1,5 @@
 def main():
-
+    percent_fuel= percent() * 100
 
     if percent_fuel == 0:
         print('E')
@@ -12,40 +12,19 @@ def percent():
     try:
         s = input("What fractions? ")
         x,y = s.split('/')
-        x = int(x)
-        y = int(y)
-        fraction = x/y
-
-
-
-
-
-
-
-def main ():
-    final_fuel = get_percent()
-    percent_fuel = round(final_fuel * 100)
-    if percent_fuel <= 1:
-        print("E")
-    elif percent_fuel >= 99:
-        print("F")
-    else:
-        print(f"{percent_fuel}%")
-
-
-def get_percent():
-    try:
-        while True:
-            fraction = input("Fuel Fraction: ")
-            x, y = fraction.split('/')
-            x = int(x)
-            y = int(y)
-            fuel = x/y
-            if fuel <= 1:
-                return fuel
+        x = float(x)
+        y = float(y)
+        fraction = x / y
+        if fraction <= 1:
+                return fraction
+        else:
+            pass
 
     except (ValueError, ZeroDivisionError):
         pass
 
+main()
 
-main ()
+
+
+
