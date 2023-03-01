333d84336fd3460c24fecfba87d403a1487e53de
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-01 10:58:34 +0000

    Wed, Mar 1, 2023, 1:58 PM +03:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,15 +1,4 @@
 def main ():
-    d = {
-        "Baja Taco": 4.00,
-        "Burrito": 7.50,
-        "Bowl": 8.50,
-        "Nachos": 11.00,
-        "Quesadilla": 8.50,
-        "Super Burrito": 8.50,
-        "Super Quesadilla": 9.50,
-        "Taco": 3.00,
-        "Tortilla Salad": 8.00
-            }
 
     while True:
         try:
@@ -20,6 +9,17 @@ def main ():
             pass
     print(f'{s.values}%')
 
+d = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+        }
 
 
 
