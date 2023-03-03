f0a8ec87a0370b83bd8822c9f984b61cab57181e
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 12:53:15 +0000

    Fri, Mar 3, 2023, 3:53 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,36 +1,34 @@
-def main():
-  months =  [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-    while True:
-        date = input('Date: ')
-        try:
-            month, day, year = date.split('/')
-            if(1<= int(month)<= 12) and (1<= int(day)<=31):
-              break
-        except:
-            try:
-                old_m, old_d, old_y = date.split(' ')
-                for i in range(len(months)):
-                    if old_m ==  month[i]:
-                        month = i + 1
-                 day = old_d.replace(',', '')
-                 if ( 1<= int(month) <= 12) and ( 1<= int(day) <=31):
-                    break
-                except:
-                 print()
-                 pass
-    print(f'(year)-(int(month):02)-(int(day):02)')
 
-main()
+months =  [
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December"
+]
+while True:
+    date = input('Date: ')
+    try:
+        month, day, year = date.split('/')
+        if(1<= int(month)<= 12) and (1<= int(day)<=31):
+            break
+    except:
+        try:
+            old_m, old_d, old_y = date.split(' ')
+            for i in range(len(months)):
+                if old_m ==  month[i]:
+                    month = i + 1
+                day = old_d.replace(',', '')
+                if ( 1<= int(month) <= 12) and ( 1<= int(day) <=31):
+                    break
+        except:
+                print()
+                pass
+print(f'(year)-(int(month):02)-(int(day):02)')
