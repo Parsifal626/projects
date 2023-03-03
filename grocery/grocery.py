bd1620ccaa2197ff0fee8b79d6cf670b9a3cf3db
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 09:45:02 +0000

    Fri, Mar 3, 2023, 12:45 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,16 +1,10 @@
-
+def main():
     s = input('List: ').upper().split(' ')
     x, y = s()
     x = int(x)
     y = str(y)
     count = 0
-    for i in x:
-        count += x
-        return (count)
+    if 
 
 
-
-
-
-
-
+main()
