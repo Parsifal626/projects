934496d78d11f6643c6c87ffe7fd91210739aad2
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 10:11:58 +0000

    Fri, Mar 3, 2023, 1:11 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,14 +1,16 @@
+def main():
+    grocery = {}
 
-grocery = {}
+    while True:
+        try:
+            s = input().lower()
+            if s in grocery:
+                grocery[s] += 1
+            else:
+                grocery[s] = 1
+        except EOFError:
+            for key in sorted(grocery.keys()):
+                print(grocery[key], key.upper())
+            break
 
-while True:
-    try:
-        s = input('List: ').lower()
-        if s in grocery:
-            grocery[s] += 1
-        else:
-            grocery[s] = 1
-    except EOFError:
-        for key in sorted(grocery.keys()):
-            print(grocery[key], key.upper())
-        break
+main()
