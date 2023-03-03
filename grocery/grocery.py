35bfb2bb8f332658e630a4ca00487ee173c11037
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 09:38:29 +0000

    Fri, Mar 3, 2023, 12:38 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,5 +1,14 @@
 def main():
-    s = input('List: ').upper()
+    s = input('List: ').upper().split(' ')
+    x, y = s()
+    x = int(x)
+    y = str(y)
+count = 0
+    for x in s():
+        count += x
+    return count
+
+
 
 
 
