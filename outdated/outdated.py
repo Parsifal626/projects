7a893f8579450d724fb0564e5daf6c8a40368867
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 13:17:49 +0000

    Fri, Mar 3, 2023, 4:17 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,4 +1,3 @@
-
 months =  [
 "January",
 "February",
@@ -16,7 +15,7 @@ months =  [
 while True:
     date = input('Date: ')
     try:
-        month, day, year = date.split('/')
+        month,day,year = date.split('/')
         if(1<= int(month)<= 12) and (1<= int(day)<=31):
             break
     except:
@@ -31,5 +30,5 @@ while True:
         except:
             print()
             pass
-        
+
 print(f'{year}-{int(month):02}-{int(day):02}')
