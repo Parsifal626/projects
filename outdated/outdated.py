17c48f0c93b11f1c7f4e03dad79c6dde50c33b72
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 13:53:13 +0000

    Fri, Mar 3, 2023, 4:53 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -24,7 +24,7 @@ while True:
             for i in range(len(months)):
                 if old_m ==  months[i]:
                     month = i + 1
-                    day = old_d.replace(',', '')
+            day = old_d.replace(',', '')
             if(int(month) >=1  and int(month)<= 12) and (int(day) >=1 and int(day)<=31):
                 break
         except:
