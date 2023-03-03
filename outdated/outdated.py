b31d2424b9dde9a3124047f65df71bba5b6bfd56
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 14:10:13 +0000

    Fri, Mar 3, 2023, 5:10 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,52 +1,34 @@
-months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-  ]
+months =  [
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December"
+]
 while True:
+    date = input('Date: ')
+    try:
+        month,day,year = date.split('/')
+        if(int(month) >=1  and int(month)<= 12) and (int(day) >=1 and int(day)<=31):
+            break
+    except:
+        try:
+            old_m, old_d, year = date.split(' ')
+            for i in range(len(months)):
+                if old_m ==  months[i]:
+                    month = i + 1
+            day = old_d.replace(',', '')
+            if(int(month) >=1  and int(month)<= 12) and (int(day) >=1 and int(day)<=31):
+                break
+        except:
+            print()
+        pass
 
-      format = input("Date: ")
-      if "/" in format:
-          format = format.split("/")
-          try:
-              day = int(format[1])
-              month = int(format[0])
-          except ValueError: # When string letters are converted to numbers
-              pass
-          else:
-              day = int(format[1])
-              year = format[2]
-              if day > 31:
-                  continue
-              elif month > 12:
-                  continue
-              year = format[2]
-              month = format[0]
-              day = format[1]
-              print(f"{year}-{month.zfill(2)}-{day.zfill(2)}")
-              break
-      elif " " in format:
-          format = format.split(",")
-          year = format[1]
-          (month, day) = format[0].split(" ")
-          try:
-              month = months.index(month) + 1
-              day = int(day)
-          except IndexError:
-              pass
-          except ValueError:
-              pass
-          else:
-              if day > 31:
-                  continue
-              print(f"{year}-{month:02}-{day:02}")
-              break
+print(f"{year}-{int(month):02}-{int(day):02}")
