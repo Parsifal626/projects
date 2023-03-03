55adea2915f6c2ba4246a6ab15a3326b6c5306cc
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-03 12:22:04 +0000

    Fri, Mar 3, 2023, 3:22 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,5 +1,20 @@
 def main():
-    
+  s =  [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+    for idx, mon in enumerate(s):
 
 
 
