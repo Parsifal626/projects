dc02fbc400b8087338fb1d7b9808cb54776b447a
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-05 12:56:36 +0000

    Sun, Mar 5, 2023, 3:56 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,6 +9,7 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 3 and (sys.argv[1] == '-f' or sys.argv[1] == '--font'):
     isRandomFont = False
 else:
+    print("Invalid usage")
     sys.exit(1)
 
 #List of fonts
