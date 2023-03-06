dd6c9d727b8a8e406b4ad0c039a9eb66f9ac0cbf
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-06 08:17:51 +0000

    Mon, Mar 6, 2023, 11:17 AM +03:00

--- game/game.py
+++ game/game.py
@@ -1,13 +1,36 @@
-
+import random
 # input level of prompts
 # if x<0 should prompt again
 
 #random integer between 1 an 100
 
 while True:
-    level = input("")
+    level = input("Level: ")
 
-    try:
+    if (int(level) < 0 or not level.isdecimal()):
+        level = input("Level: ")
+
+    guess = input("Guess: ")
+    if not int(guess.isdecimal()) or int(guess) < 0:
+        guess = input("Guess: ")
+
+
+    number = random.randint(1, int(level))
+
+    if int(guess) > number:
+        print("Too large!")
+    elif int(guess) < number:
+        print("Too small!")
+    else:
+        print("Just right!")
+        break
+
+
+
+
+
+
+
 
 
 
