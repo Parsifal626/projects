184d4ef02b0866f549feaf3596edf5fc0393569c
Parsifal626 <115568934+Parsifal626@users.noreply.github.com>
2023-03-06 08:51:24 +0000

    Mon, Mar 6, 2023, 11:51 AM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -17,7 +17,17 @@ def get_level():
 
 
 def generate_integer(level):
-    ...
+    if level == 1:
+        x = random.randint(0,9)
+        y = random.randint(0,9)
+    elif level == 2:
+        x = random.randint(10,99)
+        y = random.randint(10,99)
+    else level == 3:
+        x = random.randint(100,999)
+        y = random.randint(100,999)
+    return x,y
+
 
 
 if __name__ == "__main__":
